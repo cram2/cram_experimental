cd494e6bca170aa765a10a55a018ef369c8e330d
Gayane Kazhoyan <kazhoyan@cs.uni-bremen.de>
2014-03-04 20:52:34 +0100

    Moved VISIBILITY_COSTMAP here from CRAM_PHYSICS.

---
+++ visibility_costmap/build/devel/etc/catkin/profile.d/05.catkin-test-results.sh
@@ -0,0 +1,4 @@
+# generated from catkin/cmake/env-hooks/05.catkin-test-results.sh.develspace.in
+
+export CATKIN_TEST_RESULTS_DIR="/home/gaya/workspace/rosbuild/cram_physics/visibility_costmap/build/test_results"
+export ROS_TEST_RESULTS_DIR="$CATKIN_TEST_RESULTS_DIR"
