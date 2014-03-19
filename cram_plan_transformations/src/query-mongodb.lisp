d9450c6404ccae7601510b4a0d7a46f49ea63d39
Gayane Kazhoyan <kazhoyan@cs.uni-bremen.de>
2014-03-19 20:31:53 +0100

    [plan_transforms] got rid of namespaces in json predicates, makes it more general...

--- cram_plan_transformations/src/query-mongodb.lisp
+++ cram_plan_transformations/src/query-mongodb.lisp
@@ -10,8 +10,8 @@
 (defun test-query ()
   (force-ll
    (json-prolog:prolog
-    `(and ("mongo_prolog:add_world_clauses")
-          ("mongo_prolog:model" ?model)
+    `(and ("add_world_clauses")
+          ("model" ?model)
           (= ?model "mug")
           ("get_model_pose" "mug" 0 ?pose))
     :package :cram-plan-transformations)))
